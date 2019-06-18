01353cf1fee13f153753e84fdcc2da1e35803f2f
CraZY <ayang879@gmail.com>
2019-06-18 16:54:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
-import Game from "./component/game";
+import Game from "./component/game/game";
+import CarouselDemo from "./component/carousel/index";
 // import logo from './logo.svg';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootHandler from "./reducer";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 const store = createStore(rootHandler);
 
 class App extends Component {
 	render() {
 		return (
-			<Provider store={store}>
-				<Game />;
-			</Provider>
+			<Switch>
+				<Provider store={store}>
+					{/* <Route path="/" component={Game} /> */}
+					<Route path="/game" component={Game} />
+					<Route path="/carousel" component={CarouselDemo} />
+				</Provider>
+			</Switch>
+			// <Game />
+			// {/* <Carousel>
+			// 	<Item />
+			// </Carousel> */}
 		);
 	}
 }
