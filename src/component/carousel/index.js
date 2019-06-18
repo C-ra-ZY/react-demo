ddeaba7cbdcc30e828e0e517baf757cde60739a5
CraZY <ayang879@gmail.com>
2019-06-18 16:57:22 +0800

    Create index.js

---
+++ src/component/carousel/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Carousel from "./carousel";
+
+const CarouselDemo = () => {
+	let length = 4;
+	return (
+		<Carousel style={{ width: "60vw" }}>
+			{new Array(length).fill(null).map((e, index) => {
+				return (
+					<div
+						key={index}
+						style={{
+							color: "white",
+							width: "100%",
+							userSelect: "none",
+							height: "100%",
+							backgroundColor: `rgb(0,0,${255 / length * (index + 1)})`
+						}}
+					>
+						{index}
+					</div>
+				);
+			})}
+		</Carousel>
+	);
+};
+
+export default CarouselDemo;
